574b637e82a553d97e3728f1b3cfd140873bff4b
jsiraco <83827081+jsiraco@users.noreply.github.com>
2021-11-09 13:55:47 -0500

    added mongo atlas uri

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb+srv://JSiraco:rootroot@cluster0.iabgj.mongodb.net/galleryDB?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
